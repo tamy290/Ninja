f094f727f3f5a62e38f556c2bdda1b6f50c5562f
Tamara <lau.tami5@gmail.com>
2024-06-18 14:33:25 -0400

    [TAREA] sensei

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-class Ninja{
-    constructor(nombre,salud){
-        this.nombre = nombre ;
-        this.salud = salud ;
+class Ninja {
+    constructor(nombre, salud) {
+        this.nombre = nombre;
+        this.salud = salud;
         this.velocidad = 3;
         this.fuerza = 3;
     }
@@ -12,13 +12,33 @@ class Ninja{
 
     showStats() {
         console.log(`Nombre: ${this.nombre}, Salud: ${this.salud}, Fuerza: ${this.fuerza}, Velocidad: ${this.velocidad}`);
-}
-drinkSake(){
-    this.salud += 10;
-}
+    }
+    drinkSake() {
+        this.salud += 10;
+    }
 
 }
 
-const ninja1 = new Ninja ("Hyabusa");
+const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+
+console.log(".........................");
+
+class Sensei extends Ninja {
+    constructor(nombre) {
+        super(nombre, 200);
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.")
+  }
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
